b7838e1f7ce8c48598c84acc1b0c85ea5107b257
ricklen <ricklenderink93@gmail.com>
2017-12-09 13:38:14 +0100

    TestCommit

--- 6.1 Flow control.py
+++
@@ -1,8 +0,0 @@
-for i in range(1, 12):
-    print("No. {0} squared is {1:4} and cubed is {2:4}".format(i, i ** 2, i ** 4))
-
-for i in range(1, 12):
-    print("NO {} squared is {} and cubed is {:4}".format(i, i ** 2, i ** 4))
-
-
-
